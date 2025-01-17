06aad3d53b6c82b4dc79ae6d420202899565c44e
hamim <hamim2114@gmail.com>
2025-01-17 15:07:00 +0600

    updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -237,7 +237,7 @@ const Login = (props) => {
           width: '150px',
           mb: 2
         }}>
-          <img width='100%' src="Logo.svg" alt="" />
+          <img width='100%' src="/logo.gif" alt="" />
         </Box>
         <Box />
       </Stack>
@@ -331,7 +331,7 @@ const Login = (props) => {
                   width: { xs: '70%', md: '200px' },
                   mb: 2
                 }}>
-                  <img width='100%' src="Logo.svg" alt="" />
+                  <img width='100%' src="/logo.gif" alt="" />
                 </Box>
               </Stack>
               <Typography sx={{ fontWeight: 600, fontSize: '25px', mb: 3 }}>Logg inn på kontoen din</Typography>
